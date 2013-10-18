10244b0679d778b9fde41330f6ade36d85c3802a
Daniel Jaouen <dcj24@cornell.edu>
2013-10-17 21:31:05 -0400

    Modify bootstrap.sh

--- topics/bin/bootstrap.sh
+++ topics/bin/bootstrap.sh
@@ -10,17 +10,26 @@ xcode-select --install
 # Download and install Homebrew
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 
+# Modify the PATH
+export PATH=/usr/local/bin:$PATH
+
 # Download and install git
 brew install git
 
 # Download and install Python
 brew install python --framework --with-brewed-openssl
 
-# Download and install Ansible
-brew install ansible
-
 # Download and install Mercurial
 pip install mercurial
+
+# Make the code directory
+mkdir -p ~/src
+
+# Download and install Ansible
+git clone git://github.com/danieljaouen/ansible.git ~/src/ansible
+
+# Use the forked Ansible
+source ~/src/ansible/hacking/env-setup
 
 # Clone down the Ansible repo
 git clone git://github.com/danieljaouen/ansible-base-osx.git ~/.ansible.d
