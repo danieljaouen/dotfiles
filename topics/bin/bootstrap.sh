5f555ebb920d3e1a56d1ba9f14c495616c0ecd50
Daniel Jaouen <dcj24@cornell.edu>
2013-10-18 00:12:08 -0400

    bootstrap.sh

--- topics/bin/bootstrap.sh
+++ topics/bin/bootstrap.sh
@@ -2,43 +2,71 @@
 
 set -e
 
+SRC_DIRECTORY="$HOME/src"
+ANSIBLE_DIRECTORY="$SRC_DIRECTORY/ansible"
+ANSIBLE_CONFIGURATION_DIRECTORY="$HOME/.ansible.d"
+
 # Prerequisites: local SSH server
 
 # Download and install Command Line Tools
-xcode-select --install
+if [[ ! -x /usr/bin/gcc ]]; then
+    echo "Info   | Install   | xcode"
+    xcode-select --install
+fi
 
 # Download and install Homebrew
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+if [[ ! -x /usr/local/bin/brew ]]; then
+    echo "Info   | Install   | homebrew"
+    ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+fi
 
 # Modify the PATH
 export PATH=/usr/local/bin:$PATH
 
+# Download and install zsh
+if [[ ! -x /usr/local/bin/zsh ]]; then
+    echo "Info   | Install   | zsh"
+    brew install zsh
+fi
+
 # Download and install git
-brew install git
+if [[ ! -x /usr/local/bin/git ]]; then
+    echo "Info   | Install   | git"
+    brew install git
+fi
 
-# Download and install Python
-brew install python --framework --with-brewed-openssl
+# Download and install python
+if [[ ! -x /usr/local/bin/python ]]; then
+    echo "Info   | Install   | python"
+    brew install python --framework --with-brewed-openssl
+fi
 
-# Download and install Mercurial
-pip install mercurial
+# Download and install hg
+if [[ ! -x /usr/local/bin/hg ]]; then
+    pip install mercurial
+fi
 
 # Make the code directory
-mkdir -p ~/src
+mkdir -p $SRC_DIRECTORY
 
-# Download and install Ansible
-git clone -b brew git://github.com/danieljaouen/ansible.git ~/src/ansible
+# Clone down ansible
+if [[ ! -d $ANSIBLE_DIRECTORY ]]; then
+    git clone -b brew git://github.com/danieljaouen/ansible.git $ANSIBLE_DIRECTORY
+fi
+
+# Check out the 'brew' branch
+(cd $ANSIBLE_DIRECTORY && git checkout brew)
 
 # Use the forked Ansible
-source ~/src/ansible/hacking/env-setup
+source $ANSIBLE_DIRECTORY/hacking/env-setup
 
 # Clone down the Ansible repo
-git clone git://github.com/danieljaouen/ansible-base-osx.git ~/.ansible.d
-
-# Clone down the Ansible repo
-git clone git://github.com/danieljaouen/ansible-base-osx.git ~/.ansible.d
+if [[ ! -d $ANSIBLE_CONFIGURATION_DIRECTORY ]]; then
+    git clone git://github.com/danieljaouen/ansible-base-osx.git $ANSIBLE_CONFIGURATION_DIRECTORY
+fi
 
 # Provision the box
-ansible-playbook -i ~/.ansible.d/osx ~/.ansible.d/site.yml
+ansible-playbook -i $ANSIBLE_CONFIGURATION_DIRECTORY/inventories/osx $ANSIBLE_CONFIGURATION_DIRECTORY/site.yml
 
 # Link the casks.
 ~/.bin/link-casks
