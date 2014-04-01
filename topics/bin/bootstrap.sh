81f04236b2547214d0e5fec20a1a6d2381703ebf
Daniel Jaouen <dcj24@cornell.edu>
2014-04-01 11:10:47 -0400

    Update bootstrap.sh

--- topics/bin/bootstrap.sh
+++ topics/bin/bootstrap.sh
@@ -56,12 +56,8 @@ mkdir -p $SRC_DIRECTORY
 
 # Clone down ansible
 if [[ ! -d $ANSIBLE_DIRECTORY ]]; then
-    git clone -b devel git://github.com/ansible/ansible.git $ANSIBLE_DIRECTORY
+    git clone -b devel git://github.com/danieljaouen/ansible.git $ANSIBLE_DIRECTORY
 fi
-
-
-# Check out the 'brew' branch
-(cd $ANSIBLE_DIRECTORY && git checkout brew)
 
 # Use the forked Ansible
 source $ANSIBLE_DIRECTORY/hacking/env-setup > /dev/null
