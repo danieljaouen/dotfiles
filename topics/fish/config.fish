ae3bc705268f49e4dfd4e5c392b5d8d5d7346b90
Daniel Jaouen <dcj24@cornell.edu>
2016-05-12 20:46:06 -0400

    Update config.fish

--- topics/fish/config.fish
+++ topics/fish/config.fish
@@ -1,6 +1,21 @@
 # setup ------------------------------------------------------------------- {{{
-set fish_path $HOME/.oh-my-fish
-source $fish_path/oh-my-fish.fish
+# set fish_path $HOME/.oh-my-fish
+# source $fish_path/oh-my-fish.fish
+
+# Path to oh-my-fish.
+set -g OMF_PATH $HOME/.local/share/omf
+
+# Path to oh-my-fish configuration.
+set -g OMF_CONFIG $HOME/.config/omf
+
+### Configuration required to load oh-my-fish ###
+# Note: Only add configurations that are required to be set before oh-my-fish
+# is loaded.
+# For common configurations, we advise you to add them to your
+# $OMF_CONFIG/init.fish file or to create a custom plugin instead.
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
 
 # Global Options and Aliases --------------------------------------- {{{
 set -x EDITOR vim
