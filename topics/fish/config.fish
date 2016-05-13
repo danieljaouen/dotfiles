b488f6998fd72b228e69966499326aa7e78d7532
Daniel Jaouen <dcj24@cornell.edu>
2016-05-12 20:50:45 -0400

    Update config.fish

--- topics/fish/config.fish
+++ topics/fish/config.fish
@@ -2,21 +2,6 @@
 # set fish_path $HOME/.oh-my-fish
 # source $fish_path/oh-my-fish.fish
 
-# Path to oh-my-fish.
-set -g OMF_PATH $HOME/.local/share/omf
-
-# Path to oh-my-fish configuration.
-set -g OMF_CONFIG $HOME/.config/omf
-
-### Configuration required to load oh-my-fish ###
-# Note: Only add configurations that are required to be set before oh-my-fish
-# is loaded.
-# For common configurations, we advise you to add them to your
-# $OMF_CONFIG/init.fish file or to create a custom plugin instead.
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-
 # Global Options and Aliases --------------------------------------- {{{
 set -x EDITOR vim
 set -x TERM screen-256color-bce
@@ -86,15 +71,29 @@ switch (uname)
 end
 # /PATH ------------------------------------------------------------------- }}}
 
-# Plugins and Themes ----------------------------------------------- {{{
+# oh-my-fish setup ------------------------------------------------- {{{
+# Path to oh-my-fish.
+set -g OMF_PATH $HOME/.local/share/omf
+
+# Path to oh-my-fish configuration.
+set -g OMF_CONFIG $HOME/.config/omf
+
+### Configuration required to load oh-my-fish ###
+# Note: Only add configurations that are required to be set before oh-my-fish
+# is loaded.
+# For common configurations, we advise you to add them to your
+# $OMF_CONFIG/init.fish file or to create a custom plugin instead.
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+
 # Plugin "brew"
 # Plugin "bundler"
 # Plugin "django"
 # Plugin "pyenv"
 # Plugin "rbenv"
 # Theme "danieljaouen"
-# /Plugins and Themes ---------------------------------------------- }}}
-# /setup ------------------------------------------------------------------ }}}
+# /oh-my-fish setup ------------------------------------------------ }}}
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
