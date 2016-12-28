6c9d59154a8012d3dacda65127d03711bfe1549d
Daniel Jaouen <dcj24@cornell.edu>
2016-12-27 19:55:04 -0500

    Update topics/fish/config.fish

--- topics/fish/config.fish
+++ topics/fish/config.fish
@@ -116,8 +116,8 @@ switch (uname)
         # aws -------------------------------- {{{
         set -x AWS_CREDENTIAL_FILE "$HOME/.boto"
         set -x JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home/jre
-        set -x EC2_PRIVATE_KEY (/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)
-        set -x EC2_CERT (/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)
+        # set -x EC2_PRIVATE_KEY (/bin/ls $HOME/.ec2/pk-*.pem | /usr/bin/head -1)
+        # set -x EC2_CERT (/bin/ls $HOME/.ec2/cert-*.pem | /usr/bin/head -1)
         set -x EC2_HOME "/usr/local/Cellar/ec2-api-tools/1.6.12.0/libexec"
         # /aws ------------------------------- }}}
     case Linux
