976503061787d2b4ee9a23dc7b849f33d8d523e3
Daniel Jaouen <dcj24@cornell.edu>
2017-11-19 08:29:21 -0500

    Add remove-trailing-whitespace to spacemacs

--- topics/emacs/spacemacs.el
+++ topics/emacs/spacemacs.el
@@ -348,6 +348,7 @@ you should place your code here."
     (define-key evil-normal-state-map "\C-j" 'evil-window-down)
     (define-key evil-normal-state-map "\C-k" 'evil-window-up)
     (define-key evil-normal-state-map "\C-l" 'evil-window-right)
+    (define-key evil-normal-state-map ",w" 'delete-trailing-whitespace)
     (add-hook 'before-save-hook 'delete-trailing-whitespace)
   )
 
