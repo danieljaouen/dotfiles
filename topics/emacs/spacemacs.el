656dbfdb397f9af3d34a33fb7a13a76ff20e2045
Daniel Jaouen <dcj24@cornell.edu>
2017-11-19 19:36:21 -0500

    Testing magit

--- topics/emacs/spacemacs.el
+++ topics/emacs/spacemacs.el
@@ -333,7 +333,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-    ; (require 'column-marker)
+    (require 'column-marker)
     (require 'helm)
     ; (require 'key-chord)
     ; (key-chord-mode 1)
