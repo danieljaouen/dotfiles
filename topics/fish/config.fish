50fcc5dba54f6d7340eb8d1a3040978f5f0887fa
Daniel Jaouen <dcj24@cornell.edu>
2017-11-21 01:31:20 -0500

    Add pyenv back

--- topics/fish/config.fish
+++ topics/fish/config.fish
@@ -360,21 +360,21 @@ set -x VIRTUAL_ENV_DISABLE_PROMPT 0
 # pyenv -------------------------------------- {{{
 # . "(pyenv init -)"
 # . "(pyenv virtualenv-init -)"
-# set -gx PATH "$HOME/.pyenv/shims" $PATH
-# setenv PYENV_SHELL fish
-# . "$HOME/.pyenv/libexec/../completions/pyenv.fish"
-# command pyenv rehash 2>/dev/null
-# function pyenv
-#     set command $argv[1]
-#     set -e argv[1]
-#
-#     switch "$command"
-#     case rehash shell
-#         . (pyenv "sh-$command" $argv|psub)
-#     case '*'
-#         command pyenv "$command" $argv
-#     end
-# end
+set -gx PATH "$HOME/.pyenv/shims" $PATH
+setenv PYENV_SHELL fish
+. "$HOME/.pyenv/libexec/../completions/pyenv.fish"
+command pyenv rehash 2>/dev/null
+function pyenv
+    set command $argv[1]
+    set -e argv[1]
+
+    switch "$command"
+    case rehash shell
+        . (pyenv "sh-$command" $argv|psub)
+    case '*'
+        command pyenv "$command" $argv
+    end
+end
 # /pyenv ------------------------------------- }}}
 
 function daspt
