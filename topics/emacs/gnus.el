ad6b247cd3596203ecbd90cee71c4c559ad82391
Daniel Jaouen <dcj24@cornell.edu>
2017-11-25 12:27:41 -0500

    gnus gmail configuration

---
+++ topics/emacs/gnus.el
@@ -0,0 +1,12 @@
+(setq user-mail-address "dcj24@cornell.edu"
+      user-full-name "Daniel Jaouen")
+
+(setq gnus-select-method
+      '(nnimap "gmail"
+               (nnimap-address "imap.gmail.com")
+               (nnimap-server-port "imaps")
+               (nnimap-stream ssl)))
+
+(setq smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
