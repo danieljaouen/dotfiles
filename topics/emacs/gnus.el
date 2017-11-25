c34deeb5480b9fb0622709266bbbaeaecc28900a
Daniel Jaouen <dcj24@cornell.edu>
2017-11-25 12:40:06 -0500

    gnus bindings

--- topics/emacs/gnus.el
+++ topics/emacs/gnus.el
@@ -10,3 +10,9 @@
 (setq smtpmail-smtp-server "smtp.gmail.com"
       smtpmail-smtp-service 587
       gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
+
+(define-key gnus-group-mode-map "\C-h" 'evil-window-left)
+(define-key gnus-group-mode-map "\C-j" 'evil-window-down)
+(define-key gnus-group-mode-map "\C-k" 'evil-window-up)
+(define-key gnus-group-mode-map "\C-l" 'evil-window-right)
+(define-key gnus-group-mode-map ";" 'evil-ex)
