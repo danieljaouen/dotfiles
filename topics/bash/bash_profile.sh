6acd0ffa1a7255b4ee107754502af86cf1a206d8
Daniel Jaouen <dcj24@cornell.edu>
2017-11-30 08:35:45 -0500

    Add bash_profile and bashrc

---
+++ topics/bash/bash_profile.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export PATH=$HOME/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+# Setting PATH for Python 3.6
+# The original version is saved in .bash_profile.pysave
+PATH="/Library/Frameworks/Python.framework/Versions/3.6/bin:${PATH}"
+export PATH
---
+++ topics/bash/bashrc.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+export PATH=$HOME/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
