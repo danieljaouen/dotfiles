e6d9be8972178b63a542d1b30a48843a18114780
Daniel Jaouen <dcj24@cornell.edu>
2017-12-05 13:30:02 -0500

    Initialize pyenv and rbenv in bash*

--- topics/bash/bash_profile.sh
+++ topics/bash/bash_profile.sh
@@ -6,3 +6,23 @@ export PATH=$HOME/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/
 # The original version is saved in .bash_profile.pysave
 PATH="${HOME}/.bin:${HOME}/.pyenv/bin:${HOME}/.rbenv/bin:/Library/Frameworks/Python.framework/Versions/3.6/bin:${PATH}"
 export PATH
+
+# rbenv:
+export PATH="/Users/dan/.rbenv/shims:${PATH}"
+export RBENV_SHELL=bash
+source '/Users/dan/.rbenv/libexec/../completions/rbenv.bash'
+command rbenv rehash 2>/dev/null
+rbenv() {
+    local command
+    command="${1:-}"
+    if [ "$#" -gt 0 ]; then
+        shift
+    fi
+
+    case "$command" in
+        rehash|shell)
+            eval "$(rbenv "sh-$command" "$@")";;
+        *)
+            command rbenv "$command" "$@";;
+    esac
+}
--- topics/bash/bashrc.sh
+++ topics/bash/bashrc.sh
@@ -1,3 +1,43 @@
 #!/usr/bin/env bash
 
 export PATH=$HOME/.bin:$HOME/.pyenv/bin:$HOME/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+# rbenv:
+export PATH="/Users/dan/.rbenv/shims:${PATH}"
+export RBENV_SHELL=bash
+source '/Users/dan/.rbenv/libexec/../completions/rbenv.bash'
+command rbenv rehash 2>/dev/null
+rbenv() {
+    local command
+    command="${1:-}"
+    if [ "$#" -gt 0 ]; then
+        shift
+    fi
+
+    case "$command" in
+        rehash|shell)
+            eval "$(rbenv "sh-$command" "$@")";;
+        *)
+            command rbenv "$command" "$@";;
+    esac
+}
+
+# pyenv:
+export PATH="/Users/dan/.pyenv/shims:${PATH}"
+export PYENV_SHELL=bash
+source '/Users/dan/.pyenv/libexec/../completions/pyenv.bash'
+command pyenv rehash 2>/dev/null
+pyenv() {
+    local command
+    command="$1"
+    if [ "$#" -gt 0 ]; then
+        shift
+    fi
+
+    case "$command" in
+        rehash|shell)
+            eval "$(pyenv "sh-$command" "$@")";;
+        *)
+            command pyenv "$command" "$@";;
+    esac
+}
