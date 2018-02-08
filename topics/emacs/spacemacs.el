d39c555f80321c779bc972bcb1abc8dce7b46532
Daniel Jaouen <dcj24@cornell.edu>
2018-02-07 19:45:34 -0500

    Set sentence-end-double-space to t in spacemacs

--- topics/emacs/spacemacs.el
+++ topics/emacs/spacemacs.el
@@ -352,6 +352,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (setq sentence-end-double-space t)
   )
 
 (defun dotspacemacs/user-config ()
