3bdfabd6467be6f07a1c3b6250a6eb3be8d5bb8a
Daniel Jaouen <dcj24@cornell.edu>
2018-02-14 23:51:21 -0500

    Add asdf to bash{_profile,rc}

--- topics/bash/bash_profile.sh
+++ topics/bash/bash_profile.sh
@@ -2,6 +2,8 @@
 
 export PATH=$HOME/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+. ~/.asdf/asdf.sh
+
 # Setting PATH for Python 3.6
 # The original version is saved in .bash_profile.pysave
 PATH="${HOME}/.bin:${HOME}/.pyenv/bin:${HOME}/.rbenv/bin:/Library/Frameworks/Python.framework/Versions/3.6/bin:${PATH}"
--- topics/bash/bashrc.sh
+++ topics/bash/bashrc.sh
@@ -2,6 +2,8 @@
 
 export PATH=$HOME/.bin:$HOME/.pyenv/bin:$HOME/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+. ~/.asdf/asdf.sh
+
 # rbenv ------------------------------------------------------------------- {{{
 export PATH="/Users/dan/.rbenv/shims:${PATH}"
 export RBENV_SHELL=bash
