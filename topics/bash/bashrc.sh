c686e3a996414eecfd53947e0803ac17ebd23e0f
Daniel Jaouen <dcj24@cornell.edu>
2023-03-21 18:21:19 -0400

    Add fzf keybindings

--- topics/bash/bashrc.sh
+++ topics/bash/bashrc.sh
@@ -52,3 +52,5 @@ source "$HOME/.cargo/env"
 
 # Fig post block. Keep at the bottom of this file.
 [[ -f "$HOME/.fig/shell/bashrc.post.bash" ]] && builtin source "$HOME/.fig/shell/bashrc.post.bash"
+
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
