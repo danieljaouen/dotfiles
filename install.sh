60266e0e9452bf3072605b2d71180bbd821eeac8
Daniel Jaouen <dcj24@cornell.edu>
2023-03-23 22:50:35 -0400

    Add to install.sh and zshrc

--- install.sh
+++ install.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 
+# prerequisites:
+# - zsh
+# - emacs
+# - vim
+# - nvim
+# - asdf
+# - erlang
+# - elixir
+# - tmux
+# - ruby
+# - rake
+# - starship
+# - fzf (and its install script)
+
 mkdir -p ~/.config
 mkdir -p ~/.tmux/bundle
 mkdir -p ~/src
 
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/syl20bnr/spacemacs.git ~/.emacs.d
 git clone https://github.com/danieljaouen/AstroNvim.git ~/.config/nvim
 git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 git clone https://github.com/elixir-lsp/elixir-ls.git ~/src/elixir-ls
