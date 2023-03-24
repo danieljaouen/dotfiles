3932adaf07c7bc9a7413d42176c3432147eb0b24
Daniel Jaouen <dcj24@cornell.edu>
2023-03-23 22:31:15 -0400

    Set up elixir_ls in install.sh

--- install.sh
+++ install.sh
@@ -9,4 +9,12 @@ git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 git clone https://github.com/elixir-lsp/elixir-ls.git ~/src/elixir-ls
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
+cd ~/src/elixir-ls
+mix local.hex --force
+mix local.rebar --force
+mix deps.get
+mix compile
+mix elixir_ls.release -o release
+cd -
+
 # remember to remove ~/.config/nvim/init.vim !!
