1cbdd7bdacc37f8bba046a236e39d0d9593269bf
Daniel Jaouen <dcj24@cornell.edu>
2024-03-14 05:45:56 -0400

    Add EXWM

--- topics/emacs/spacemacs.el
+++ topics/emacs/spacemacs.el
@@ -52,6 +52,7 @@ values."
      emoji
      erlang
      ess
+     exwm
      git
      gnus
      go
@@ -403,6 +404,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+    (exwm-enable)
     (load "~/.spacemacs.d/requires")
     (load "~/.spacemacs.d/defuns")
     (load "~/.spacemacs.d/vars")
