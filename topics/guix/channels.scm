80052d0055f4a3c57d7ba6a1d9e2da24a219e37b
Daniel Jaouen <dcj24@cornell.edu>
2024-03-22 02:02:51 -0400

    Modify guix channels.scm

--- topics/guix/channels.scm
+++ topics/guix/channels.scm
@@ -7,4 +7,14 @@
           "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
           (openpgp-fingerprint
            "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+
+       (channel
+        (name 'glarus)
+        (url "https://code.glarus.dev/guix/channel.git")
+        (introduction
+         (make-channel-introduction
+          "08f6249fba55c9aed16a8ffd897b28c823044b1f"
+          (openpgp-fingerprint
+           "9E23 D5BC AEB7 E364 7321  4A00 C3A7 8EB3 FEFE 6C04"))))
+
        %default-channels)
