2d3953b8cc296c9e0753334ced2b67d036922ab6
Daniel Jaouen <dcj24@cornell.edu>
2024-06-27 01:43:43 -0400

    Remove glarus channel from guix channels

--- topics/guix/channels.scm
+++ topics/guix/channels.scm
@@ -8,13 +8,4 @@
           (openpgp-fingerprint
            "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
 
-       (channel
-        (name 'glarus)
-        (url "https://code.glarus.dev/guix/channel.git")
-        (introduction
-         (make-channel-introduction
-          "08f6249fba55c9aed16a8ffd897b28c823044b1f"
-          (openpgp-fingerprint
-           "9E23 D5BC AEB7 E364 7321  4A00 C3A7 8EB3 FEFE 6C04"))))
-
        %default-channels)
